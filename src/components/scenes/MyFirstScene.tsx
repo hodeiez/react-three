2fd4f85dc7280a993ed8e8d7246bf2ba37921aac
hodeiez <hodeinaiz@hotmail.com>
2022-10-02 23:58:59 +0200

    added simple server time sky show

--- src/components/scenes/MyFirstScene.tsx
+++ src/components/scenes/MyFirstScene.tsx
@@ -7,10 +7,20 @@ import { MyStars } from "components/environment/MyStars";
 import { MySky } from "components/environment/MySky";
 
 export function MyFirstScene() {
+  const time = new Date().getHours();
+  const val = {
+    rayleigh: time > 20 || time < 7 ? 6.2 : 0.6,
+    mieCoefficient: 0.009,
+    mieDirectionalG: 0.01,
+  };
   return (
     <>
       <OrbitControls />
-      <MySky />
+      <MySky
+        rayleigh={val.rayleigh}
+        mieCoefficient={val.mieCoefficient}
+        mieDirectionalG={val.mieDirectionalG}
+      />
       <MyStars />
       <MyText text="hodei" />
     </>
